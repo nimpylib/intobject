5de868ec470e78818f5dac45c22ea81407b30863
litlighilit <litlighilit@foxmail.com>
2026-03-14 05:27:35 +0800

    fixup(968f63179): bit_length wrong&not compile when  noUndefinedBitops or for 0

--- src/intobject/bit_length_util.nim
+++ src/intobject/bit_length_util.nim
@@ -4,9 +4,9 @@ export popcount
 const BitPerByte = 8
 proc bit_length*(self: SomeInteger): int =
   when defined(noUndefinedBitOpts):
-    sizeof(x) * BitPerByte bitops.countLeadingZeroBits x
-  else:
     1 + fastLog2(
       when self is SomeSignedInt: abs(self)
       else: self
     )
+  else:
+    sizeof(self) * BitPerByte - bitops.countLeadingZeroBits self
